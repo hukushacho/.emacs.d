ffc52d0bf141ee10e7dffd728676ad36833c38ac
shacho <issei_k@rd5.so-net.ne.jp>
2016-03-07 00:07:45 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,18 @@
+;; ---------------------------------------------------------
+;; YaTeX のèå
+;; ---------------------------------------------------------
+   
+;; Add library path
+(add-to-list 'load-path "~/.emacs.d/site-lisp/yatex/yatex1.78.4")
+;; YaTeX mode
+(setq auto-mode-alist
+    (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+(setq tex-command "platex")
+(setq dviprint-command-format "dvipdfmx %s")
+;; use Preview.app
+(setq dvi2-command "open -a Preview")
+(defvar YaTeX-dvi2-command-ext-alist    
+  '(("xdvi" . ".dvi")                   
+      ("ghostview\\|gv" . ".ps")
+      ("acroread\\|pdf\\|Preview\\|open" . ".pdf")))
